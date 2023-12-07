4e34e563bcc4441e396c8d2ae009e35246c97a97
doddy-s <doddy.s@students.amikom.ac.id>
2023-12-07 07:47:41 +0700

    creates branch for back-end using sqlite

--- database/database.go
+++ database/database.go
@@ -1,10 +1,7 @@
 package database
 
 import (
-	"fmt"
-	"os"
-
-	"gorm.io/driver/mysql"
+	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 
 	"amikom-hacktalent-be/models"
@@ -15,22 +12,11 @@ var (
 )
 
 func ConnectDB() {
-	DB_USER := os.Getenv("MYSQL_USER")
-	DB_PASSWORD := os.Getenv("MYSQL_PASSWORD")
-	DB_DBNAME := os.Getenv("MYSQL_DBNAME")
-	DB_HOST := os.Getenv("MYSQL_HOST")
-	DB_PORT := "3306"
-
-	var err error
-	dsn := DB_USER + ":" + DB_PASSWORD + "@tcp" + "(" + DB_HOST + ":" + DB_PORT + ")/" + DB_DBNAME + "?" + "parseTime=true&loc=Local"
-	// fmt.Println("dsn : ", dsn)
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-
+	db, err := gorm.Open(sqlite.Open("database.db"), &gorm.Config{})
 	if err != nil {
-		os.Exit(1)
+		panic("failed to connect database")
 	}
 
 	db.AutoMigrate(&models.User{}, &models.Schedule{})
-	fmt.Println("Database connected, as " + DB_USER + " on " + DB_HOST + " with database " + DB_DBNAME)
 	DBConn = db
 }
